de0a833fc3cb41f28d196cd8b1ef97c901696676
RohitSenta <rohitsenta@gmail.com>
2023-08-31 00:24:07 +0530

    Figlet Package

---
+++ FigletDir/Index.js
@@ -0,0 +1,10 @@
+const figlet = require("figlet");
+
+figlet("Rohit Senta!!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });

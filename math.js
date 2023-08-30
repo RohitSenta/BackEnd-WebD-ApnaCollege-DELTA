1798e08cd5c10f5f8794bcfa6926e1b97dc04fe7
RohitSenta <rohitsenta@gmail.com>
2023-08-31 00:18:54 +0530

    require and import with expot

--- math.js
+++ math.js
@@ -25,7 +25,12 @@
 // module.exports.g = 9.7;
 // module.exports.PI = 3.14;
 
-exports.sum = (a, b) => a + b;
-exports.mul = (a, b) => a * b;
-exports.g = 9.7;
-exports.PI = 3.14;
+// exports.sum = (a, b) => a + b;
+// exports.mul = (a, b) => a * b;
+// exports.g = 9.7;
+// exports.PI = 3.14;
+
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const g = 9.7;
+export const PI = 3.14;
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let mathObj = require("./math");
+// let mathObj = require("./math");
 
-console.log(mathObj);
+// console.log(mathObj);
+
+import {sum, mul, PI} from "./math.js";
+
+console.log(sum(2, 3));
+console.log(mul(2, 3));
+console.log(PI);
+
+import { generate } from "random-words";
+
+console.log(generate());

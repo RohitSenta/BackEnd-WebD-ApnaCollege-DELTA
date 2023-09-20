6732b50494c00af10b33fe421424bed965d63638
RohitSenta <rohitsenta@gmail.com>
2023-09-20 16:50:45 +0530

    Export From Directory

---
+++ Basics_Of_NodeJS/Fruits/index.js
@@ -0,0 +1,7 @@
+const apple = require("./apple");
+const mango = require("./mango");
+const orange = require("./orange");
+
+let fruits = [apple, mango, orange];
+
+module.exports = fruits;
---
+++ Basics_Of_NodeJS/fruitsInfo.js
@@ -0,0 +1,10 @@
+const fruitsInfo = require("./Fruits");
+
+console.log(fruitsInfo);
+
+console.log(fruitsInfo[0].name);
+console.log(fruitsInfo[0].color);
+console.log(fruitsInfo[1].name);
+console.log(fruitsInfo[1].color);
+console.log(fruitsInfo[2].name);
+console.log(fruitsInfo[2].color);

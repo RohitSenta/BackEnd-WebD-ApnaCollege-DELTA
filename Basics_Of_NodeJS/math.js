5c2e32d947a7ead519eb19b25e94d437cccda025
RohitSenta <rohitsenta@gmail.com>
2023-09-20 16:48:30 +0530

    Module.exports, Import And Require Concept

---
+++ Basics_Of_NodeJS/math.js
@@ -0,0 +1,35 @@
+// const sum = (a, b) => a + b;
+// const mul = (a, b) => a * b;
+// const g = 9.7;
+// const PI = 3.14;
+ 
+// const obj = { 
+//     sum: sum(2, 3),
+//     mul: mul(2, 3),
+//     g: g, 
+//     PI: PI
+// }
+
+// module.exports = obj;
+
+// module.exports = {
+//     sum: sum(2, 3),
+//     mul: mul(2, 3),
+//     g: g, 
+//     PI: PI
+// }
+
+// module.exports.sum = (a, b) => a + b;
+// module.exports.mul = (a, b) => a * b;
+// module.exports.g = 9.7;
+// module.exports.PI = 3.14;
+
+// exports.sum = (a, b) => a + b;
+// exports.mul = (a, b) => a * b;
+// exports.g = 9.7;
+// exports.PI = 3.14;
+
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const g = 9.7;
+export const PI = 3.14;
---
+++ Basics_Of_NodeJS/script.js
@@ -0,0 +1,13 @@
+// let mathObj = require("./math");
+
+// console.log(mathObj);
+
+import {sum, mul, PI} from "./math.js";
+
+console.log(sum(2, 3));
+console.log(mul(2, 3));
+console.log(PI);
+
+import { generate } from "random-words";
+
+console.log(generate());
